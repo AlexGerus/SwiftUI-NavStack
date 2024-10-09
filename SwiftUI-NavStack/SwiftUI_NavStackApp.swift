baec153c2bd4d123a0a9e64322a38471baa2238a
Alex Gerus <alexander.gerus@grse.de>
2024-10-09 14:09:52 +0300

    Init

---
+++ SwiftUI-NavStack/SwiftUI_NavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavStackApp.swift
+//  SwiftUI-NavStack
+//
+//  Created by Alexander Gerus on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
